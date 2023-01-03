ea064825ca6fa831957b921c383d2512b6ebd92a
karintou8710 <karintou8710@gmail.com>
2023-01-04 00:49:23 +0900

    [add] migrate command

--- command.go
+++ command.go
@@ -10,9 +10,11 @@ type ScrapingCommand struct {
 	} `positional-args:"yes" required:"yes"`
 }
 type UpdateGradeCommand struct{}
+type MigrateCommand struct{}
 
 var updateGradeCommand UpdateGradeCommand
 var scrapingCommand ScrapingCommand
+var migrateCommand MigrateCommand
 
 func (cmd *ScrapingCommand) Execute(args []string) error {
 	return controllers.ScrapingHandler(cmd.Positional.Year, cmd.Positional.Semester, cmd.Positional.FacultyID)
@@ -22,10 +24,17 @@ func (cmd *UpdateGradeCommand) Execute(args []string) error {
 	return controllers.UpdateGradeHandler()
 }
 
+func (cmd *MigrateCommand) Execute(args []string) error {
+	return controllers.MigrateHandler()
+}
+
 func init() {
 	const scrapingDescription = "Scraping from North University's grade distribution site and saving as a CSV file."
 	parser.AddCommand("scraping", scrapingDescription, scrapingDescription, &scrapingCommand)
 
 	const updateGradeDescription = "Updating the grade distribution table in the database with CSV files."
 	parser.AddCommand("updateGrade", updateGradeDescription, updateGradeDescription, &updateGradeCommand)
+
+	const migrateDescription = "Create a table of grade distribution."
+	parser.AddCommand("migrate", migrateDescription, migrateDescription, &migrateCommand)
 }
--- controllers/migrateHandler.go
+++ controllers/migrateHandler.go
@@ -1,11 +1,11 @@
-package main
+package controllers
 
 import (
 	"karintou8710/iNAZO-scraping/database"
 	"karintou8710/iNAZO-scraping/models"
 )
 
-func migrate() error {
+func MigrateHandler() error {
 	db := database.GetDB()
 	err := db.AutoMigrate(&models.GradeDistribution{})
 	if err != nil {
